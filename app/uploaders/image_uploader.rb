48be7c921805c5ba971c3b7f7b0b8be1faa12f9b
uchiborieri <zuumo219@gmail.com>
2023-05-31 17:00:50 +0900

    Logoutボタンエラー修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
+  version :thumb do
   storage :file
   process resize_to_limit: [50, 50]
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
